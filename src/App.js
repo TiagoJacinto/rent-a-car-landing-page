3aed1bfaf1f8c937a24bb4ff55f4dd637c53c7c6
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-09-27 08:33:37 +0100

    Add Components To App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import Hero from './components/Hero/Hero';
+import Hero from './components/Hero';
+import SelectCar from './components/SelectVehicle';
+import DeliverFeature from './components/DeliverFeature';
+import TradeFeature from './components/TradeFeature';
+import OtherFeatures from './components/OtherFeatures';
+import Testimonial from './components/Testimonial';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <>
       <Hero />
+      <SelectCar />
+      <DeliverFeature />
+      <TradeFeature />
+      <OtherFeatures />
+      <Testimonial />
+      <Footer />
     </>
   );
 }
